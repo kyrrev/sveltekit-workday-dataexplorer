2f11c7f628ba913084647d4fbbdb56c5f4689531
Kyrre <kyrre@me.com>
2023-03-30 11:20:57 +0200

    Deploy bugfix

--- src/routes/jobcatalogue/+page.server.ts
+++ src/routes/jobcatalogue/+page.server.ts
@@ -10,8 +10,8 @@ import {
 
 // Log out environment variables for troubleshooting
 
-console.log("BASE_URL:", BASE_URL);
-console.log("TOKEN_ENDPOINT:", TOKEN_ENDPOINT);
+console.log("BASE_URL:", process.env.BASE_URL);
+console.log("TOKEN_ENDPOINT:", process.env.TOKEN_ENDPOINT);
 console.log("CLIENT_ID:", CLIENT_ID);
 console.log("CLIENT_SECRET:", CLIENT_SECRET);
 console.log("REFRESH_TOKEN:", REFRESH_TOKEN);
