77fe4868cef68a5ba0afec5838c1fbf7dd62dba0
Kyrre <kyrre@me.com>
2023-04-11 21:27:34 +0200

    Error handling on server function

--- src/routes/jobcatalogue/+page.server.ts
+++ src/routes/jobcatalogue/+page.server.ts
@@ -33,52 +33,60 @@ interface JobProfile {
 
 // This async function gets API data and returns it
 export async function load(): Promise<{ jobProfiles: JobProfile[] }> {
-	// Fetch access token from token endpoint
-	const token_response = await fetch(TOKEN_ENDPOINT, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded',
-			Authorization: 'Basic ' + Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64')
-		},
-		body: 'grant_type=refresh_token&refresh_token=' + REFRESH_TOKEN
-	});
-	const token_data = await token_response.json();
-	const access_token = token_data.access_token;
-	let jobProfilesRaw: any[] = [];
-	let total = 0;
-	const limit = 100;
-	let offset = 0;
-	while (total === 0 || offset < total) {
-		// Fetching data from API at BASE_URL with query parameters and access token
-		const response = await fetch(
-			BASE_URL + '/data?' + '&limit=' + limit + '&offset=' + offset + '&query=' + QUERY_PARAM,
-			{
-				method: 'GET',
-				headers: {
-					Authorization: 'Bearer ' + access_token
+	try {
+		// Fetch access token from token endpoint
+		const token_response = await fetch(TOKEN_ENDPOINT, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+				Authorization: 'Basic ' + Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64')
+			},
+			body: 'grant_type=refresh_token&refresh_token=' + REFRESH_TOKEN
+		});
+		const token_data = await token_response.json();
+		const access_token = token_data.access_token;
+		let jobProfilesRaw: any[] = [];
+		let total = 0;
+		const limit = 100;
+		let offset = 0;
+		while (total === 0 || offset < total) {
+			// Fetching data from API at BASE_URL with query parameters and access token
+			const response = await fetch(
+				BASE_URL + '/data?' + '&limit=' + limit + '&offset=' + offset + '&query=' + QUERY_PARAM,
+				{
+					method: 'GET',
+					headers: {
+						Authorization: 'Bearer ' + access_token
+					}
 				}
+			);
+			if (!response.ok) {
+				throw new Error(`Failed to fetch API data (HTTP status ${response.status})`);
 			}
-		);
-		const data = await response.json();
-
-		jobProfilesRaw = jobProfilesRaw.concat(data.data);
-		total = data.total;
-		offset += limit;
-	}
-	console.log('jobProfilesRaw:', jobProfilesRaw);
+			const data = await response.json();
 	
-	// Mapping the data to a flat structure and removing the IDs
-	const jobProfiles: JobProfile[] = jobProfilesRaw.map((item) => {
-		return {
-			cf_CFESIJobFamilyGroup: item.cf_CFESIJobFamilyGroup?.descriptor || 'Uncategorized',
-			cf_CFESIJobFamilyGroupID: item.cf_CFESIJobFamilyGroup?.id || '',
-			cf_CFESIJobFamily: item.cf_CFESIJobFamily?.descriptor || 'Uncategorized',
-			cf_CFESIJobFamilyID: item.cf_CFESIJobFamily?.id || '',
-			jobProfile: item.jobProfile?.descriptor || '',
-			jobProfileID: item.jobProfile?.id || '',
-			managementLevel: item.managementLevel?.descriptor || '',
-			workerCountWithJobProfile: item.workerCountWithJobProfile
-		};
-	});
-	return { jobProfiles };
+			jobProfilesRaw = jobProfilesRaw.concat(data.data);
+			total = data.total;
+			offset += limit;
+		}
+		console.log('jobProfilesRaw:', jobProfilesRaw);
+		
+		// Mapping the data to a flat structure and removing the IDs
+		const jobProfiles: JobProfile[] = jobProfilesRaw.map((item) => {
+			return {
+				cf_CFESIJobFamilyGroup: item.cf_CFESIJobFamilyGroup?.descriptor || 'Uncategorized',
+				cf_CFESIJobFamilyGroupID: item.cf_CFESIJobFamilyGroup?.id || '',
+				cf_CFESIJobFamily: item.cf_CFESIJobFamily?.descriptor || 'Uncategorized',
+				cf_CFESIJobFamilyID: item.cf_CFESIJobFamily?.id || '',
+				jobProfile: item.jobProfile?.descriptor || '',
+				jobProfileID: item.jobProfile?.id || '',
+				managementLevel: item.managementLevel?.descriptor || '',
+				workerCountWithJobProfile: item.workerCountWithJobProfile
+			};
+		});
+		return { jobProfiles };
+	} catch (error) {
+		console.error(error);
+		throw new Error('Failed to load data');
+	}
 }
